1bb2a61e0a1ba44d89f34f2f0361dd7175607183
Piero Coronado Flores <pacoronado01@gmail.com>
2026-02-16 13:47:01 -0500

    Update codebreaker ui to lecture 2

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
    var body: some View {
-       VStack {
-           Image(systemName: "globe")
-           Text("greetings!")
-           Text("howdy!")
-           Circle()
-       }
+       VStack{
+           pegs(colors: [.red, .green, .green, .yellow])
+           pegs(colors: [.red, .blue, .green, .red])
+           pegs(colors: [.red, .yellow, .green, .blue])
+
+       }.padding()
        
+    }
+    
+    func pegs(colors: Array<Color>) -> some View {
+        HStack {
+            ForEach(colors.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 10)
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(colors[index])
+            }
+            MatchMarkers(matches: [.exact, .inexact, .nomatch, .exact])
+        }
     }
 }
 
